09f790f0cc97483ded636872b3b27f424a9593fe
LAPTOP-0PJOI0SQ\Lenovo <jemontoyaca@unal.edu.co>
2023-04-27 19:05:52 -0500

    Pruebas

--- app/polls/models.py
+++ app/polls/models.py
@@ -1,10 +1,25 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 class Question(models.Model):
     question=models.CharField(max_length=200)
     pub_date=models.DateTimeField("date published")
 
+    def __str__(self):
+        return self.question
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    class Meta:
+        ordering = ["pk"]
+        verbose_name_plural = "Preguntas de la Encuesta"
+
 class Choice(models.Model):
     question=models.ForeignKey(Question,on_delete=models.CASCADE)
     choice_text=models.CharField(max_length=200)
     votes=models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
+    class Meta:
+        verbose_name_plural = "Opciones de la Encuesta"
+    
